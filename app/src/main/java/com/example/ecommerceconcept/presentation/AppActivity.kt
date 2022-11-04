4311c2f659dd17e101626ab701f9276fe9def988
ParshinSA <Parshin.Sergey222@mail.ru>
2022-11-04 18:58:43 +0300

    end started settings

---
+++ app/src/main/java/com/example/ecommerceconcept/presentation/AppActivity.kt
@@ -0,0 +1,14 @@
+package com.example.ecommerceconcept.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.ecommerceconcept.R
+
+class AppActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_app)
+    }
+
+}
---
+++ network/src/main/java/com/example/network/di/NetworkModule.kt
@@ -0,0 +1,26 @@
+package com.example.network.di
+
+import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+class NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    companion object {
+        const val BASE_URL = "https://run.mocky.io/"
+    }
+}
